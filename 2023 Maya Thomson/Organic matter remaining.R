c60e2e0d15b69f70aa029b5abb9ea2e8caff52a8
PEGutierrF <pgutierr@uvm.edu>
2024-01-25 15:40:02 -0500

    data

--- 2023 Maya Thomson/Organic matter remaining.R
+++ 2023 Maya Thomson/Organic matter remaining.R
@@ -31,3 +31,35 @@ ggplot(data_remaining, aes(x = Group, y = Mean_remaining, fill = Stream)) +
        y = "Mean organic matter remaining (g)") +
   theme_classic()
 
+
+
+###########################################################################
+# ANOVA -------------------------------------------------------------------
+###########################################################################
+
+data_anova <- data %>%
+  group_by(Stream, Pack, Replicate) %>%
+  slice(1) %>%
+  ungroup() %>%
+  select(Stream, Weight_final)
+
+# Print the new table
+print(data_anova)
+
+shapiro.test(data_anova$Weight_final)
+new_Weight_final <- log(data_anova$Weight_final+1)
+shapiro.test(new_Weight_final)
+
+
+# Perform one-way ANOVA
+result_anova <- aov(new_Weight_final ~ Stream, data = new_table)
+
+# Display the ANOVA table
+summary(result_anova)
+
+# Perform Tukey post-hoc test
+tukey_result <- TukeyHSD(result_anova)
+
+# Display the Tukey post-hoc test results
+print(tukey_result)
+plot(tukey_result)
