302ce4f7650eb0a4561a36f99fbfa59225f85a7d
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2024-01-28 11:20:43 -0500

    data

--- 2023 Maya Thomson/Functional Feeding Groups.R
+++ 2023 Maya Thomson/Functional Feeding Groups.R
@@ -10,14 +10,41 @@ library(patchwork)
 data <- read_excel('2023 Maya Thomson/Sum_Stream_Data.xlsx', sheet = 'FFG')
 head(data,6)
 
-# Set the desired order for the stream variable
-data$stream <- factor(data$stream, levels = c("Brown", "Stevenville", "Potash", "Muddy"))
+# Filtrar el dataframe para Pair1 (Brown y Stevenville)
+Forested <- data[data$stream %in% c("Brown", "Stevenville"), ]
+
 
 # Create a 100% stacked bar chart
-ggplot(data, aes(x = stream, y = value, fill = ffg)) +
+f <- ggplot(Forested, aes(x = stream, y = value, fill = ffg)) +
   geom_bar(stat = "identity", position = "stack") +
-  labs(title = "",
+  geom_text(aes(label = paste0(round(value,1))),
+            size = 5, position = position_stack(vjust = 0.5),
+            colour = "black") +
+  labs(title = "Forested streams",
        y = "Percentage",
-       x = "") 
+       x = "")  +
+  theme_classic() +
+  theme(legend.position = "none")
+f
+
+
+# Filtrar el dataframe para Pair2 (Potash y Muddy)
+urban <- data[data$stream %in% c("Potash", "Muddy"), ]
+
+# Crear un 100% stacked bar chart con porcentajes en el eje y y etiquetas de texto
+u <- ggplot(urban, aes(x = stream, y = value, fill = ffg)) +
+  geom_bar(stat = "identity", position = "stack") +
+  geom_text(aes(label = paste0(round(value,1))),
+            size = 5, position = position_stack(vjust = 0.5),
+            colour = "black") +
+  labs(title = "Urban streams",
+       y = "Percentage",
+       x = "") +
+  theme_classic() +
+  scale_y_continuous(labels = scales::percent_format(scale = 1))
+
+u
+
+f + u
 
 
