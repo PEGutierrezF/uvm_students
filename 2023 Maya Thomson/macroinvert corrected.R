52740f7b3e8f265ce59fe8b00975d3d2c760fcb3
PEGutierrF <pgutierr@uvm.edu>
2024-03-25 12:44:46 -0400

    data

--- 2023 Maya Thomson/macroinvert corrected.R
+++ 2023 Maya Thomson/macroinvert corrected.R
@@ -21,8 +21,6 @@ result <- data %>%
 
 result
 
-
-
 # Summarize by stream
 summary_richness <- result %>%
   group_by(Stream) %>%
@@ -32,7 +30,7 @@ summary_richness <- result %>%
 
 # Create a new column for grouping
 summary_richness$Group <- ifelse(summary_richness$Stream %in% 
-                                   c("Brown", "Stevenspeil"), "Forested", "Urban")
+                                   c("Brown", "Stevensville"), "Forested", "Urban")
 
 # Create barplot
 p_c <- ggplot(summary_richness, aes(x = Group, y = Mean_Fam_richness, fill = Stream)) +
@@ -58,13 +56,16 @@ anova_richness <- result %>%
   ungroup() %>%
   select(Stream, total_corrected)
 
+anova_richness <- anova_richness %>%
+  mutate(landuse = ifelse(Stream %in% c("Brown", "Stevensville"), "Forested", "Urban"))
+
 shapiro.test(anova_richness$total_corrected)
 new_total_corrected <- 1/sqrt((anova_richness$total_corrected))
 shapiro.test(new_total_corrected)
 
 
 # Perform one-way ANOVA
-result_anova <- aov(new_total_corrected ~ Stream, data = result)
+result_anova <- aov(new_total_corrected ~ landuse*Stream, data = anova_richness)
 
 # Display the ANOVA table
 summary(result_anova)
@@ -93,7 +94,7 @@ summary_abundance <- result_abundance %>%
 
 # Create a new column for grouping
 summary_abundance$Group <- ifelse(summary_abundance$Stream %in% 
-                                    c("Brown", "Stevenspeil"), "Forested", "Urban")
+                                    c("Brown", "Stevensville"), "Forested", "Urban")
 
 # Create barplot for abundance
 q_c <- ggplot(summary_abundance, aes(x = Group, y = mean_abundance, fill = Stream)) +
@@ -103,15 +104,14 @@ q_c <- ggplot(summary_abundance, aes(x = Group, y = mean_abundance, fill = Strea
   labs(title = "",
        x = "Stream",
        y = "Mean Abundance") +
-  theme_classic()
+  theme_classic()+
+  theme(legend.position = "none")
 
 q_c
 
 
 
 p_c + q_c
-
-
 
 
 
@@ -124,18 +124,84 @@ anova_abundance <- result_abundance %>%
   ungroup() %>%
   select(Stream, abunndance_corrected)
 
+anova_abundance <- anova_abundance %>%
+  mutate(landuse = ifelse(Stream %in% c("Brown", "Stevensville"), "Forested", "Urban"))
+
 shapiro.test(anova_abundance$abunndance_corrected)
 new_abunndance_corrected<-log(anova_abundance$abunndance_corrected)
 shapiro.test(new_abunndance_corrected)
 
 
 # Perform one-way ANOVA
-result_anova <- aov(new_abunndance_corrected ~ Stream, data = result)
+result_anova <- aov(new_abunndance_corrected ~ landuse * Stream, data = anova_abundance)
 
 # Display the ANOVA table
 summary(result_anova)
 
 # Perform Tukey post-hoc test
-tukey_result <- TukeyHSD(result_anova)
+TukeyHSD(result_anova)
 
+
+
+
+# Biomass -----------------------------------------------------------------
+# Sum biomass column by Stream and Pack
+result_biomass <- data %>%
+  group_by(Stream, Pack) %>%
+  summarize(biomass = sum(biomass),
+            FirstWeightFinal = first(Weight_final),
+            biomass_corrected = biomass / FirstWeightFinal)
+
+result_biomass
+
+
+# Calculate mean and standard deviation for each stream
+summary_biomass <- result_biomass %>%
+  group_by(Stream) %>%
+  summarize(mean_biomass = mean(biomass_corrected),
+            sd_biomass = sd(biomass_corrected))
+
+
+# Create a new column for grouping
+summary_biomass$Group <- ifelse(summary_biomass$Stream %in% 
+                                    c("Brown", "Stevensville"), "Forested", "Urban")
+
+
+
+# Create barplot for abundance
+biomass_c <- ggplot(summary_biomass, aes(x = Group, y = mean_biomass, fill = Stream)) +
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  geom_errorbar(aes(ymin = mean_biomass, ymax = mean_biomass + sd_biomass),
+                position = position_dodge(0.9), width = 0.2) +
+  labs(title = "",
+       x = "Stream",
+       y = "Mean Biomass (mg AFDM/g)") +
+  theme_classic()
+
+p_c + q_c + biomass_c
+
+
+
+anova_biomass <- result_biomass %>%
+  group_by(Stream, Pack) %>%
+  slice(1) %>%
+  ungroup() %>%
+  select(Stream, biomass_corrected)
+
+anova_biomass <- anova_biomass %>%
+  mutate(landuse = ifelse(Stream %in% c("Brown", "Stevensville"), "Forested", "Urban"))
+
+shapiro.test(anova_biomass$biomass_corrected)
+new_biomass_corrected <- 1/sqrt((anova_biomass$biomass_corrected))
+shapiro.test(new_biomass_corrected)
+
+
+# Perform one-way ANOVA
+result_biomass_anova <- aov(new_biomass_corrected ~ Stream + landuse, data = anova_biomass)
+
+# Display the ANOVA table
+summary(result_biomass_anova)
+
+# Perform Tukey post-hoc test
+TukeyHSD(result_biomass_anova)
 
