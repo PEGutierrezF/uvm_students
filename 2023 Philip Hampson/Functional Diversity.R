08cc2c6cac97037682226411aeb7e9e3283e882d
PEGutierrF <pgutierr@uvm.edu>
2024-05-16 14:42:47 -0400

    data

--- 2023 Philip Hampson/Functional Diversity.R
+++ 2023 Philip Hampson/Functional Diversity.R
@@ -10,21 +10,21 @@ library(patchwork)
 library(FD)
 
 ##### Spp Matrix #####
-sp.matrix <- read.csv("spp.csv", row.names= 1)
+sp.matrix <- read.csv("functional analysis/Bug_abundance_sorted.csv")
 head(sp.matrix)
 
 #Traits Matrix
-tr.matrix<-read.csv("traits.csv", row.names=1)
+tr.matrix <- read_xlsx("functional analysis/traits.xlsx")
 head(tr.matrix)
 
 
 #Treatment Matrix
-treatment.matrix <- read.csv("treatment.csv", row.names=1)
+treatment.matrix <- read.csv("functional analysis/treatment.csv")
 head(treatment.matrix) 
 
 
 #convertir los numeros enteros en factores (para que R no entienda las categorias como numeros)
-tr.matrix.factor<-as.data.frame(apply(tr.matrix[,], 2, as.factor))
+tr.matrix.factor <- as.data.frame(apply(tr.matrix[,], 2, as.factor))
 tr.matrix.factor
 
 #Anadir nombres a las filas
@@ -32,13 +32,9 @@ rownames(tr.matrix.factor) <- rownames(tr.matrix)
 
 #Crear una matriz de distancias usando la metrica de desemejanza de Gower (u otra)
 #(usualmente usamos la distancia euclidea para rasgos continuos)
-tr.dist<-gowdis(tr.matrix.factor)
+tr.dist <- gowdis(tr.matrix.factor)
 head(tr.dist)
 
-FD.output<-dbFD(tr.dist, sp.matrix)
+FD.output <- dbFD (tr.dist, sp.matrix)
 FD.output$FRic
-
-
-
-
 
