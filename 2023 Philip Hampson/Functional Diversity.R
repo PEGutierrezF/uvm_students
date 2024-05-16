84f88c69214004f16d56b52579662bcdebf09a3a
PEGutierrF <pgutierr@uvm.edu>
2024-05-16 15:42:16 -0400

    data

--- 2023 Philip Hampson/Functional Diversity.R
+++ 2023 Philip Hampson/Functional Diversity.R
@@ -10,22 +10,19 @@ library(patchwork)
 library(FD)
 
 ##### Spp Matrix #####
-sp.matrix <- read.csv("functional analysis/Bug_abundance_sorted.csv")
+sp.matrix <- read.csv("functional analysis/Bug_abundance_sorted.csv",  row.names=1)
 head(sp.matrix)
-
 #Traits Matrix
 tr.matrix <- read_xlsx("functional analysis/traits.xlsx")
 head(tr.matrix)
-
-
 #Treatment Matrix
-treatment.matrix <- read.csv("functional analysis/treatment.csv")
+treatment.matrix <- read.csv("functional analysis/treatment.csv",  row.names=1)
 head(treatment.matrix) 
 
 
 #convertir los numeros enteros en factores (para que R no entienda las categorias como numeros)
 tr.matrix.factor <- as.data.frame(apply(tr.matrix[,], 2, as.factor))
-tr.matrix.factor
+
 
 #Anadir nombres a las filas
 rownames(tr.matrix.factor) <- rownames(tr.matrix)
@@ -35,6 +32,6 @@ rownames(tr.matrix.factor) <- rownames(tr.matrix)
 tr.dist <- gowdis(tr.matrix.factor)
 head(tr.dist)
 
-FD.output <- dbFD (tr.dist, sp.matrix)
+FD.output <- dbFD(tr.dist, sp.matrix)
 FD.output$FRic
 
