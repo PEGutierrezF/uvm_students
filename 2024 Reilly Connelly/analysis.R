3d13ce91aa47fa55b8a69c982e5dae1a6e1f26a0
PEGutierrF <pgutierr@uvm.edu>
2024-10-24 23:42:55 -0400

    data

--- 2024 Reilly Connelly/analysis.R
+++ 2024 Reilly Connelly/analysis.R
@@ -7,8 +7,8 @@ data<- read_xlsx('data.xlsx')
 head(data,6)
 
 # Create the plot
-ggplot(data, aes(x = connect, y = num_spec, 
-                 color = stream_order)) +
+ggplot(data, aes(x = num_spec, y = num_link, 
+                 color = region)) +
   geom_point(size = 3) +  
   labs(
     x = "Connectance",
@@ -17,3 +17,19 @@ ggplot(data, aes(x = connect, y = num_spec,
   ) +
   theme_minimal() +
   theme(legend.position = "top") 
+
+
+
+# Create the plot with linear models for both regions on the same plot
+b <- ggplot(data, aes(x = connect, y = num_spec, color = region)) +
+  geom_point(size = 3) +  # Points colored by region
+  geom_smooth(method = "lm", se = FALSE) +  # Add separate linear models for each region
+  labs(
+    x = "Connectance",
+    y = "Number of Species",
+    color = "Region"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "top")  # Move the legend to the top
+b
+a + b
